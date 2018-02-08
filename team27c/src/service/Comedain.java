753f69f0f5373bc9339b77296025418eb35e042e
Administrator <Administrator@MSDN-SPECIAL>
2018-02-08 16:30:12 +0900

    [임가현]comedian.java getters/setters

--- team27c/src/service/Comedain.java
+++ team27c/src/service/Comedain.java
@@ -1,5 +1,27 @@
 package service;
-
 public class Comedain {
-
+	private String CId;
+	private String CName;
+	private String CAge;
+	
+	public String getCId() {
+		return CId;
+	}
+	public void setCId(String cId) {
+		CId = cId;
+	}
+	public String getCName() {
+		return CName;
+	}
+	public void setCName(String cName) {
+		CName = cName;
+	}
+	public String getCAge() {
+		return CAge;
+	}
+	public void setCAge(String cAge) {
+		CAge = cAge;
+	}
+	
+	
 }
