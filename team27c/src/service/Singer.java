ac0a9e8be636e2ff7ca21356ca83305c7c384f10
wsj86 <wsj86@192.168.219.107>
2018-02-09 00:20:38 +0900

    [왕서준] Singer.java 수정 및 커밋완료

---
+++ team27c/src/service/Singer.java
@@ -0,0 +1,26 @@
+package service;
+
+public class Singer {
+	private String Singer_Id;
+	private String Singer_Name;
+	private String Singer_Age;
+	public String getSinger_Id() {
+		return Singer_Id;
+	}
+	public void setSinger_Id(String singer_Id) {
+		Singer_Id = singer_Id;
+	}
+	public String getSinger_Name() {
+		return Singer_Name;
+	}
+	public void setSinger_Name(String singer_Name) {
+		Singer_Name = singer_Name;
+	}
+	public String getSinger_Age() {
+		return Singer_Age;
+	}
+	public void setSinger_Age(String singer_Age) {
+		Singer_Age = singer_Age;
+	}
+	
+}
