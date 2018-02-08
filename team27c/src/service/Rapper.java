71ed7f5d28af12c7740660904aece84e75f57552
smart-202-19 <smart-202-19@smart-202-19-PC>
2018-02-08 16:37:14 +0900

    [박지하]Rapper.java업데이트

--- team27c/src/service/Rapper.java
+++ team27c/src/service/Rapper.java
@@ -2,4 +2,25 @@ package service;
 
 public class Rapper {
 
+	private String RapperId;
+	private String RapperName;
+	private String RapperAge;
+	public String getRapperId() {
+		return RapperId;
+	}
+	public void setRapperId(String rapperId) {
+		RapperId = rapperId;
+	}
+	public String getRapperName() {
+		return RapperName;
+	}
+	public void setRapperName(String rapperName) {
+		RapperName = rapperName;
+	}
+	public String getRapperAge() {
+		return RapperAge;
+	}
+	public void setRapperAge(String rapperAge) {
+		RapperAge = rapperAge;
+	}
 }
