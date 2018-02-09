9ccf8b81bc807dcfc8f0c3da945fb3515e06d0b2
201-13 <201-13@201-13-PC>
2018-02-09 11:19:49 +0900

    [왕서준] SingerDao 최종 수정

--- team27c/src/service/SingerDao.java
+++ team27c/src/service/SingerDao.java
@@ -1,52 +1,63 @@
 package service;
 
+import service.Singer;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-
-import service.Singer;
-
+import java.sql.ResultSet;
 
 public class SingerDao {
-	
-	public static void main(String[] agrs) {
-	
-	}
+
+	public ArrayList<Singer> selectSinger(){
+		ArrayList<Singer> arrlist = new ArrayList<Singer>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 		
-		public ArrayList<Singer> selectSingerList() throws ClassNotFoundException, SQLException{
-			
-			ArrayList<Singer> arrSinger = new ArrayList<Singer>();
-			Connection conn = null;
-			PreparedStatement pstmt = null;
-			ResultSet rs = null;
-		
+	
+		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			String jdbcDriver = "jdbc:mysql://localhost:3306/jjdev?" +
-					"useUnicode=true&characterEncoding=euckr";
+
+			String jdbcDriver = "jdbc:mysql://localhost:3306/jjdev? useUnicode=true&characterEncoding=euckr";
 			String dbUser = "root";
 			String dbPass = "java0000";
+
 			conn = DriverManager.getConnection(jdbcDriver, dbUser, dbPass);
+			System.out.println(conn + "db¿¬°á");
 			
-			pstmt = conn.prepareStatement("SELECT * FROM Singer");
-			rs = pstmt.executeQuery();
+			pstmt = conn.prepareStatement("select singer_id,singer_name,singer_age from singer");
+			rs=pstmt.executeQuery();
 			
+			System.out.println(pstmt);
+			
+			Singer singer = null;
 			
 			while(rs.next()) {
-				Singer singer = new Singer();
-				singer.setSinger_Id(rs.getInt("Singer_Id"));
-				singer.setSinger_Name(rs.getString("Singer_Name"));
-				singer.setSinger_Age(rs.getInt("Singer_Age"));
-				arrSinger.add(singer);
+				singer = new Singer();
+				singer.setSinger_Id(rs.getInt("singer_id"));
+				singer.setSinger_Name(rs.getString("singer_name"));
+				singer.setSinger_Age(rs.getInt("singer_age"));
+				arrlist.add(singer);
 			}
-			
-			if (rs != null) try { rs.close(); } catch(SQLException ex) {}
-			if (pstmt != null) try { pstmt.close(); } catch(SQLException ex) {}
-			if (conn != null) try { conn.close(); } catch(SQLException ex) {}
-			
-			return arrSinger;
+		
+		
+			for(Singer s: arrlist) {
+				System.out.println(s.getSinger_Id());
+				System.out.println(s.getSinger_Name());
+				System.out.println(s.getSinger_Age());
+		
+			}
+		
+		}catch(ClassNotFoundException c) {
+			System.out.println("dzz");
+		}catch(SQLException s) {
+			s.printStackTrace();
 		}
+		return arrlist;
+		
+
 	}
 
+}
