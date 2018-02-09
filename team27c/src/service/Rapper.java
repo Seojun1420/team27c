4902fbac43fdb4ffd9d6ec21aede472ee7888440
smart-202-19 <smart-202-19@smart-202-19-PC>
2018-02-09 14:26:07 +0900

    [박지하]커밋을위해 다시

--- team27c/src/service/Rapper.java
+++ team27c/src/service/Rapper.java
@@ -1,29 +1,14 @@
 package service;
 
-/*import java.sql.Connection;
-import java.util.ArrayList;
-*/
 public class Rapper {
-	/*public Connection conn =null;
-	String jdbcDriver = "jdbc:mysql://localhost:3306/mysql;jjdev?" +
-			"useUnicode=true&characterEncoding=euckr";
-	String dbUser = "root";
-	String dbPass = "java0000";
-	conn = DriverManager.getConnection(jdbcDriver, dbUser, dbPass);
 	
-	public void RapperDao() ArrayList selectRapperList() {
-		return new ArrayList<>();*/
-	/*
-	}
-	*/
-/*	public ArrayList<Rapper> */
-	private String RapperId;
+	private int RapperId;
 	private String RapperName;
-	private String RapperAge;
-	public String getRapperId() {
+	private int RapperAge;
+	public int getRapperId() {
 		return RapperId;
 	}
-	public void setRapperId(String rapperId) {
+	public void setRapperId(int rapperId) {
 		RapperId = rapperId;
 	}
 	public String getRapperName() {
@@ -32,10 +17,15 @@ public class Rapper {
 	public void setRapperName(String rapperName) {
 		RapperName = rapperName;
 	}
-	public String getRapperAge() {
+	public int getRapperAge() {
 		return RapperAge;
 	}
-	public void setRapperAge(String rapperAge) {
+	public void setRapperAge(int rapperAge) {
 		RapperAge = rapperAge;
 	}
+	@Override
+	public String toString() {
+		return "Rapper [RapperId=" + RapperId + ", RapperName=" + RapperName + ", RapperAge=" + RapperAge + "]";
+	}
+	
 }
--- team27c/src/service/RapperDao.java
+++ team27c/src/service/RapperDao.java
@@ -1,15 +1,64 @@
 package service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class RapperDao {
-/*	public ArrayList<RapperDao> selectRapperList() {
+
+
+	public ArrayList<Rapper> selectRapperList() throws SQLException, ClassNotFoundException {
+		ArrayList<Rapper> list = new ArrayList<Rapper>();
 		PreparedStatement pstmt = null;
+		
+		Connection conn = null;
+		
 		ResultSet rs = null;
-		*/
+		//select rapper_id as RapperId,rapper_name as RapperName,rapper_age as RapperAge from rapper
+		
+		try {
+			//드라이버 로딩 시작
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("드라이버로딩완료");
+			//DB연결시작:포트번호,ip주소,DB명,DBid,DBpw 작성
+			String jdbcDriver = "jdbc:mysql://localhost:3306/jjdev?" + "useUnicode=true&characterEncoding=euckr";
+			String dbUser = "root";
+			String dbPass = "java0000";
+			
+			//
+			conn = DriverManager.getConnection(jdbcDriver, dbUser, dbPass);
+			System.out.println("db연결완료");
+			pstmt = conn.prepareStatement("select rapper_id as RapperId,rapper_name as RapperName,rapper_age as RapperAge from rapper");
+			//select 쿼리문에서  id,name,age를 불러오는 쿼리문써줌
+			System.out.println(pstmt+"<---pstmt");
+			
+			rs=pstmt.executeQuery();
+			System.out.println("쿼리실행완료");
+			Rapper rapper;
+			while(rs.next()) {
+				rapper = new Rapper();
+				rapper.setRapperId(rs.getInt("RapperId"));
+				rapper.setRapperName(rs.getString("RapperName"));
+				rapper.setRapperAge(rs.getInt("RapperAge"));
+				list.add(rapper);
+				
+			}
+		
+		}finally{
+			//객체 종료(close)
+			if (rs != null) try { rs.close(); } catch(SQLException ex) {}			
+			if (pstmt != null) try {pstmt.close();}catch(SQLException ex) {}
+			if (conn != null) try { conn.close(); } catch(SQLException ex) {}
+		}
+		
+		return list;
+	
+		}
+		
 		 
 	}
 	
-/*}*/
+
